9da708b852865aa979e26e9e58861d621db34f48
unknown <mmzainabidinov@gmail.com>
2023-05-12 23:17:02 +0300

    Added middleware for backend to handle token and send response back to client with id

--- server/config/db.js
+++ server/config/db.js
@@ -16,18 +16,3 @@ const connectDB = async () => {
 
 module.exports = connectDB;
 
-// const mongoose = require("mongoose");
-
-// mongoose.connect(process.env.MONGO_URL);
-
-// const connection = mongoose.connection;
-
-// connection.on("connected", () => {
-//   console.log("Mongo Db Connection Successful");
-// });
-
-// connection.on("error", (err) => {
-//   console.log("Mongo Db Connection Failed");
-// });
-
-// module.exports = connection;
--- server/middlewares/checkAuthMiddleware.js
+++ server/middlewares/checkAuthMiddleware.js
@@ -1,50 +1,18 @@
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-// // we get tokens in header i.e req
-// // what we send to frontend is via res
-// // if token and logic are fine we will call the next method
-// module.exports = (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization.split(" ")[1];
-//     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-//     const userId = decodedToken.userId;
-//     req.body.userId = userId;
-//     next();
-//   } catch (error) {
-//     console.log(error);
-//     res.status(401).send({ message: "Not authorized" });
-//   }
+module.exports = (req, res, next) => {
+  try {
+    let tokenHeader = req.headers.authorization;
+    let token = tokenHeader.split(" ")[1];
 
-//   if (!token) {
-//     res.status(401).json({ message: "Not authorized, no token found" });
-//   }
-// };
-
-// res.send({
-//     message: "You are not authenticated",
-//     data: error,
-//     success: false,
-//   });
-
-
-
-// const jwt = require("jsonwebtoken");
-
-// // we get tokens in header i.e req
-// // what we send to frontend is via res
-// // if token and logic are fine we will call the next method
-// module.exports = (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization.split(" ")[1];
-//     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-//     const userId = decodedToken.userId;
-//     req.body.userId = userId;
-//     next();
-//   } catch (error) {
-//     res.send({
-//       message: "You are not authenticated",
-//       data: error,
-//       success: false,
-//     });
-//   }
-// };
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.body = decoded;
+    next();
+  } catch (error) {
+    res.status(401).send({
+      message: "Not authorized",
+      data: error,
+      success: false,
+    });
+  }
+};
