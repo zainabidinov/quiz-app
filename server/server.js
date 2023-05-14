ccfc7e9b40bcba2308bdc76585560078d6899d71
unknown <mmzainabidinov@gmail.com>
2023-05-14 23:27:25 +0300

    Worked on rendering components within home page when clicked from sidebar navigation instead of rendering plain components in another new  page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const colors = require("colors");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
 const dbConnect = require("./config/db");
 dbConnect();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 
 const usersRoute = require("./routes/users");
@@ -16,4 +23,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
