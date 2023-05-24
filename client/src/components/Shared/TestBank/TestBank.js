f93fbd606748a68f6349e0eda76364e5df1170b6
unknown <mmzainabidinov@gmail.com>
2023-05-24 20:14:31 +0300

    Add exam rendering from database to user's role student

--- client/src/components/Shared/TestBank/TestBank.js
+++ client/src/components/Shared/TestBank/TestBank.js
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./TestBank.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useToast } from "@chakra-ui/react";
+import { Button, useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { setExam } from "../../../redux/examSlice";
 
 const TestBank = () => {
   const toast = useToast();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [examData, setExamData] = useState([]);
+  const retrievedExams = useSelector((state) => state.exam.exam);
+  console.log("Redux state: ", examData);
 
   const displayNotification = (message, status) => {
     toast({
@@ -55,6 +58,24 @@ const TestBank = () => {
       <div className="examMainContent">
         <div className="examMainContent__header">
           <h1>Available Exams</h1>
+        </div>
+
+        <div className="examMainContent__content">
+          {examData &&
+            examData.map((exam, index) => (
+              <div className="examMainContent__content-items" key={index}>
+                <h1>{exam.name}</h1>
+                <p>Number of Questions: {exam.numberOfQuestions}</p>
+                <p>Duration of Exam: {exam.numberOfQuestions}</p>
+                <Button
+                  style={{ width: "125px", height: "30px", fontSize: "15px" }}
+                  colorScheme="teal"
+                  mt={2}
+                >
+                  Attempt exam
+                </Button>
+              </div>
+            ))}
         </div>
       </div>
     </div>
