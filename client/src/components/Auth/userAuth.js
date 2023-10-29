806d520b883e52e07dc2356f47fc02b3079426a9
unknown <mmzainabidinov@gmail.com>
2023-10-29 22:40:13 +0600

    update userAuth.js

--- client/src/components/Auth/userAuth.js
+++ client/src/components/Auth/userAuth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://techquiz.onrender.com/api",
+  baseURL: "https://techquiz-api.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
