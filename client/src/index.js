a1138f4e478ddef1e83a16c6b8537d780e47a7dc
unknown <mmzainabidinov@gmail.com>
2023-10-29 12:33:52 +0600

    Deployment test #4

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import "@mdi/font/css/materialdesignicons.min.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import {disableReactDevTools} from "@fvilers/disable-react-devtools"
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
